7074b7e82c5f32865365c200575a4703e6991356
nivanov344 <72230086+nivanov344@users.noreply.github.com>
2023-03-21 13:52:02 +0200

    Update DynamicArray.hpp

--- DynamicArray.hpp
+++ DynamicArray.hpp
@@ -3,44 +3,59 @@
 class DynamicArray {
 private:
 	int* array;
-	int capacity;
-	int count;
+	unsigned capacity;
+	unsigned count;
 
 	void cleanup();
 	void copy(const DynamicArray&);
+	void steal(DynamicArray&&);
+	void allocate(unsigned);
+	void checkCapacity();
 
 public:
-	DynamicArray(int, int);
+	DynamicArray(unsigned);
+	DynamicArray(unsigned, int);
 	DynamicArray(const DynamicArray&);
 	DynamicArray& operator=(const DynamicArray&);
 	DynamicArray(DynamicArray&&);
 	DynamicArray& operator=(DynamicArray&&);
 	~DynamicArray();
 	void append(int);
-	void insert(int, int );
-	void pop();
-	void remove(int);
+	void insert(int, unsigned);
+	int pop();
+	void remove(unsigned);
 	void clear();
 	bool isEmpty();
 	int size();
-	int get(int);
-	void set(int, int);
+	int get(unsigned);
+	void set(int, unsigned);
 	int find(int);
+	
 };
 
-DynamicArray::DynamicArray(int initSize = 100, int init = 0) {
-	int* arr = new int[initSize] {init};
+DynamicArray::DynamicArray(unsigned initSize = 100) {
+	int* arr = new int[initSize];
 	array = arr;
 	capacity = initSize;
 	count = 0;
-	if (init != 0) {
-		count = initSize;
-	}
-	std::cout << "Default " << count << " " << capacity << std::endl;
+	//std::cout << "Default constructor" << count << " " << capacity << std::endl;
+}
+
+DynamicArray::DynamicArray(unsigned initSize, int init) {
+	int* arr = new int[initSize] {init};
+	array = arr;
+	capacity = initSize;
+	count = initSize;
+	/*for (int i = 0; i < initSize; ++i) {
+		array[i] = init;
+	}*/
+	//std::cout << "Param constructor" << count << " " << capacity << std::endl;
 }
 
 DynamicArray::DynamicArray(const DynamicArray& other) {
+	//cleanup();
 	copy(other);
+	//std::cout << "Copy constructor " << count << " " << capacity << std::endl;
 }
 
 DynamicArray& DynamicArray::operator=(const DynamicArray& other) {
@@ -49,15 +64,61 @@ DynamicArray& DynamicArray::operator=(const DynamicArray& other) {
 		copy(other);
 	}
 	return *this;
+	//std::cout << "Copy operator " << count << " " << capacity << std::endl;
 }
 
 DynamicArray::DynamicArray(DynamicArray&& other) {
-	
+	steal((DynamicArray&&)other);
+	//std::cout << "Move constructor " << count << " " << capacity << std::endl;
+}
+
+DynamicArray& DynamicArray::operator=(DynamicArray&& other) {
+	if (this != &other) {
+		cleanup();
+		steal((DynamicArray&&)other);
+	}
+	//std::cout << "Move operator " << count << " " << capacity << std::endl;
+	return *this;
 }
 
 DynamicArray::~DynamicArray() {
-	delete[] array;
-	std::cout << "destructor" << std::endl;
+	cleanup();
+	//std::cout << "Destructor" << std::endl;
+}
+
+void DynamicArray::append(int num) {
+	checkCapacity();
+	array[count] = num;
+	++count;
+}
+
+void DynamicArray::insert(int num, unsigned pos) {
+	if (pos < count) {
+		checkCapacity();
+		++count;
+		for (int i = count - 1; i > pos; --i) {
+			array[i] = array[i - 1];
+		}
+		array[pos] = num;
+	}
+}
+
+int DynamicArray::pop() {
+	--count;
+	return array[count];
+}
+
+void DynamicArray::remove(unsigned pos) {
+	for (int i = pos; i < count - 1; ++i) {
+		array[i] = array[i + 1];
+	}
+	--count;
+}
+
+int DynamicArray::get(unsigned pos) {
+	if (pos < count) {
+		return array[pos];
+	}
 }
 
 void DynamicArray::cleanup() {
@@ -66,14 +127,37 @@ void DynamicArray::cleanup() {
 
 void DynamicArray::copy(const DynamicArray& other) {
 	if (other.capacity > capacity) {
-		cleanup();
-		int* newArr = new int[other.capacity];
-		array = newArr;
+		allocate(other.capacity);
 	}
 	for (int i = 0; i < other.count; ++i) {
 		array[i] = other.array[i];
 	}
 	capacity = other.capacity;
 	count = other.count;
+}
+
+void DynamicArray::steal(DynamicArray&& other) {
+	array = other.array;
+	capacity = other.capacity;
+	count = other.count;
+
+	other.array = nullptr;
+}
+
+void DynamicArray::allocate(unsigned size) {
+	array = new int[size];
+}
+
+void DynamicArray::checkCapacity() {
+	assert(count <= capacity);
+	if (count == capacity) {
+		int* old = array;
+		allocate(2 * capacity);
+		for (int i = 0; i < count; ++i) {
+			array[i] = old[i];
+		}
+		capacity *= 2;
+		delete[] old;
+	}
 }
 #endif
